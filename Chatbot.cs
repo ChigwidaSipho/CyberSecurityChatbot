f069569ce4fff2066e8d82a2f302b3341c5e3a8b
ChigwidaSipho <st10488110@imconnect.edu.ZA>
2026-03-24 10:19:40 +0200

    Added Chatbot class with basic responses

--- Chatbot.cs
+++ Chatbot.cs
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace CyberSecurityChatbot
 {
+    //A simple chatbot class that provides responses based on user input
     internal class Chatbot
     {
+        public void GreetUser()
+        {
+            Console.WriteLine("====================================");
+            Console.WriteLine("  Welcome to the Cybersecurity Bot  ");
+            Console.WriteLine("====================================");
+        }
 
+        public string GetResponse(string input)
+        {
+            input = input.ToLower();
+
+            if (input.Contains("password"))
+            {
+                return "Make sure your password is strong and not easy to guess!";
+            }
+            else if (input.Contains("phishing"))
+            {
+                return "Be careful of suspicious emails asking for personal info.";
+            }
+            else if (input.Contains("safe browsing"))
+            {
+                return "Always check if a website is secure (https).";
+            }
+            else
+            {
+                return "I’m not sure about that, but stay safe online!";
+            }
+        }
     }
 }
