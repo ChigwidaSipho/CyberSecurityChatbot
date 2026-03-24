2f740a105c8614b2ac772115c64639f7595d650d
ChigwidaSipho <st10488110@imconnect.edu.ZA>
2026-03-24 10:27:16 +0200

    Connected User and Chatbot classes in Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,33 @@ namespace CyberSecurityChatbot
     {
         static void Main(string[] args)
         {
+            //A simple console application to demonstrate a cybersecurity chatbot
+
+            Chatbot bot = new Chatbot();
+
+            bot.GreetUser();
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            User user = new User(name);
+
+            Console.WriteLine($"Hello, {user.Name}! Ask me anything about cybersecurity.");
+
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+
+                string response = bot.GetResponse(input);
+                Console.WriteLine(response);
+            }
         }
     }
 }
